e46993bc77e7725418ed5739c82c0c45dfc560cf
Ralph Plumley <ralphplumley@gmail.com>
2021-01-16 13:05:28 -0500

    complete 28 Implement strStr

---
+++ 28_implement_strstr/solution.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+let strStr = function(haystack, needle) {
+    if (needle.length === 0) return 0
+    for (let i = 0; i < haystack.length; i++) {
+        if (haystack[i] === needle[0]) {
+            let index = i
+            let j = 0
+            while (j < needle.length) {
+                if (haystack[i] !== needle[j]) break
+                if (j === needle.length - 1) return index
+                i++
+                j++
+            }
+            i = index
+        }
+    }
+    return -1
+};
+
+console.log(strStr('', ''))
