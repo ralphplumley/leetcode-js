6fcae69e13f6edbca8bc5fecbe9418a943f0691e
Ralph Plumley <ralphplumley@gmail.com>
2021-02-04 12:07:10 -0500

    complete 112 Path Sum (DFS, recursion)

--- 111_Minimum_Depth_of_Binary_Tree/solution.ts
+++ 111_Minimum_Depth_of_Binary_Tree/solution.ts
@@ -1,16 +1,13 @@
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
+ class TreeNode {
+     val: number
+     left: TreeNode | null
+     right: TreeNode | null
+     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+         this.val = (val===undefined ? 0 : val)
+         this.left = (left===undefined ? null : left)
+         this.right = (right===undefined ? null : right)
+     }
+ }
 
 function minDepth(root: TreeNode | null): number {
     if (!root) return 0
