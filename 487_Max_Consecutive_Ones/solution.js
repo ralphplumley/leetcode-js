ffd4c8591b2c18e55cd156bd08ca0890695d4eec
Ralph Plumley <ralphplumley@gmail.com>
2021-03-03 16:40:50 -0500

    complete Max Consecutive Ones

---
+++ 487_Max_Consecutive_Ones/solution.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+    let longestSequence = 0
+    let left = 0
+    let right = 0
+    let numZeroes = 0
+
+    // while our window is in bounds
+    while (right < nums.length) {
+
+        // add the right most element into our window
+        if (nums[right] === 0) {
+            numZeroes++
+        }
+
+        // if our window is invald, contract our window
+        while(numZeroes === 2) {
+            if (nums[left] === 0) {
+                numZeroes--
+            }
+            left++
+        }
+
+        // update our longest sequence answer
+        longestSequence = Math.max(longestSequence, right - left + 1)
+
+        // expand our window
+        right++
+    }
+
+    return longestSequence
+
+};
