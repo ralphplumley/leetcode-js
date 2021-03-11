53d89c5806cd7c9c225a1db74f65703fe3d297fd
Ralph Plumley <ralphplumley@gmail.com>
2021-03-11 07:03:27 -0500

    stumble through 523 Continuous Subarray Sum

---
+++ 523_Continuous_Subarray_Sum/solution.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+const checkSubarraySum = function(nums, k) {
+    // create map
+    // create sum variable
+    // for each num in nums
+        // increment sum by num
+        // calculate sum%k
+        // if sum%k is in map
+            // return true
+        // else
+            // set sum%k : i in map
+
+    // return false
+
+    let map = new Map()
+    map.set(0, -1)
+    let sum = 0
+    for (let i = 0; i < nums.length; i++) {
+        let num = nums[i]
+        sum += num
+
+        if (k !== 0) {
+            sum = sum % k
+        }
+        if (map.has(sum)) {
+            if (i - map.get(sum) > 1) {
+               return true
+            }
+        } else {
+            map.set(sum, i)
+        }
+    }
+
+    return false
+};
+
+let arr = [0,0]
+let k = 0
+console.log(checkSubarraySum(arr, k))
