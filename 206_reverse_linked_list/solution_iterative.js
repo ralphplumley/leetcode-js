0005cf066dfca4c4d03cb52c6d698de08e1ec3d7
Ralph Plumley <ralphplumley@gmail.com>
2021-03-19 12:36:14 -0400

    review reverse linked list

--- 206_reverse_linked_list/solution_iterative.js
+++ 206_reverse_linked_list/solution_iterative.js
@@ -9,17 +9,31 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var reverseList = function(head) {
-    let tmp = null
-    let newHead = null
+
+function ListNode(val, next) {
+   this.val = (val===undefined ? 0 : val)
+   this.next = (next===undefined ? null : next)
+}
+
+const reverseList = function(head) {
+    let curr = null,
+        prev = null
 
     while (head) {
-        tmp = head
+        curr = head
         head = head.next
-        tmp.next = newHead
-        newHead = tmp
+        curr.next = prev
+        prev = curr
     }
 
-    return newHead
+    return prev
 
 };
+
+let root = new ListNode(1)
+root.next = new ListNode(2)
+root.next.next = new ListNode(3)
+root.next.next.next = new ListNode(4)
+root.next.next.next.next = new ListNode(5)
+
+console.log(reverseList(root))
