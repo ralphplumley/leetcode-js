84d206562f0323a59d066197ff7ec84c5bebbd0e
Ralph Plumley <ralphplumley@gmail.com>
2021-05-13 16:45:49 -0400

    complete 70 Climbing Stairs

---
+++ 70_Climbing_Stairs/solution.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const climbStairs = function(n) {
+    let memo = new Array(n+1).fill(-1)
+    return countWays(n, memo)
+};
+
+
+const countWays = function(n, memo) {
+    if (n < 0) {
+        return 0
+    } else if (n === 0) {
+        return 1
+    } else if (memo[n] > -1) {
+        return memo[n]
+    } else {
+        memo[n] = countWays(n - 1, memo) + countWays(n - 2, memo)
+        return memo[n]
+    }
+}
