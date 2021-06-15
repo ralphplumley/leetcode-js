de407c6e82e3a4535ac524a1b062143945ce90cd
Ralph Plumley <ralphplumley@gmail.com>
2021-06-14 19:52:43 -0500

    complete 300 longest increasing subsequence, brute force

---
+++ 300_longest_increasing_subsequence/1_brute_force.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+    /*
+        brute force
+        time: O(2^n)
+        space: O(n^2)
+    */
+
+    const recurse = function (nums, prev, curpos) {
+        if (curpos === nums.length) return 0
+
+        let taken = 0
+
+        if (nums[curpos] > prev) {
+            taken = 1 + recurse(nums, nums[curpos], curpos + 1)
+        }
+
+        const nottaken = recurse(nums, prev, curpos + 1)
+        return Math.max(taken, nottaken)
+    }
+
+    return recurse(nums, -Infinity, 0)
+};
+
+// !!!!! TIME LIMIT EXCEEDED !!!!!
