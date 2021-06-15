cf7f8a5e900ff0ca000a856c9070f280bb9d9bde
Ralph Plumley <ralphplumley@gmail.com>
2021-06-14 20:20:24 -0500

    complete 300 longest increasing subsequence, recursion with memo

---
+++ 300_longest_increasing_subsequence/2_recursion_with_memo.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+    /*
+        Approach: Recursion with Memo
+        Time: O(n^2) - size of recursion tree can go up to n^2
+        Space: O(n^2) - memo array of n * n is used
+    */
+
+    const recurse = function (nums, prevIdx, curpos, memo) {
+        if (curpos === nums.length) return 0
+
+        if (memo[prevIdx + 1][curpos] >= 0) {
+            return memo[prevIdx + 1][curpos]
+        }
+
+        let taken = 0
+        if (prevIdx < 0 || nums[curpos] > nums[prevIdx]) {
+            taken = 1 + recurse(nums, curpos, curpos + 1, memo)
+        }
+
+        const nottaken = recurse(nums, prevIdx, curpos + 1, memo)
+        memo[prevIdx + 1][curpos] = Math.max(taken, nottaken)
+
+        return memo[prevIdx + 1][curpos]
+    }
+
+    let memo = []
+    for (let i = 0; i < nums.length + 1; i++) {
+        const arr = new Array(nums.length).fill(-1)
+        memo.push(arr)
+    }
+
+    return recurse(nums, -1, 0, memo)
+};
+
+console.log(lengthOfLIS([10,9,2,5,3,7,101,18]))
