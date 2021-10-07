689a8cc477848fe8978e24d6491daac7587da85c
Ralph Plumley <ralphplumley@gmail.com>
2021-10-07 06:31:32 -0400

    complete 79 Word Search

--- 79_Word_Search/solution.js
+++ 79_Word_Search/solution.js
@@ -3,27 +3,41 @@
  * @param {string} word
  * @return {boolean}
  */
+
+/*
+    T.C.:  O(N*(3^L))
+        rows x cols = N cells on the board
+        L = length of word. We check 3 directions in each back track call,
+            so 3^L
+
+    S.C.:  O(L)
+        the maximum length of the call stack would be the word length
+ */
 var exist = function(board, word) {
     const ROWS = board.length,
-        COLS = board[0].length
+        COLS = board[0].length,
+        DIRS = [
+            [0,1],
+            [1,0],
+            [0,-1],
+            [-1,0]
+        ]
 
     const backtrack = (row, col, word, index) => {
-        // step 1) check the bottom case
+        // step 1) base case
         if (index >= word.length) return true
 
         // step 2) check the boundaries
         if (row < 0 || row === ROWS || col < 0 || col === COLS || board[row][col] !== word.charAt(index)) return false
 
-        // step 3) explore the neighors in DFS
+        // step 3) explore the neighbors in DFS
         let ret = false
+
         // mark the path before the next exploration
         board[row][col] = '#'
 
-        let rowOffsets = [0, 1, 0, -1]
-        let colOffsets = [1, 0, -1, 0]
-
-        for (let dir = 0; dir < 4; ++dir) {
-            ret = backtrack(row + rowOffsets[dir], col + colOffsets[dir], word, index + 1)
+        for (let dir of DIRS) {
+            ret = backtrack(row + dir[0], col + dir[1], word, index + 1)
             if (ret) break
         }
 
@@ -41,3 +55,6 @@ var exist = function(board, word) {
     return false
 };
 
+const board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
+const word = "ABCCED"
+console.log(exist(board, word))
